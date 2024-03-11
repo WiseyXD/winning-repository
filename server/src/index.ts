1b565f02e36c79a9ead185eb5364a8554b16dadb
WiseyXD <aryan.s.nag@gmail.com>
2024-03-11 21:48:48 +0530

    build : Auth Done

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,33 +3,15 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { env } from "hono/adapter";
 
-const app = new Hono<{
-    Bindings: {
-        DATABASE_URL: string;
-    };
-}>();
+import auth from "./routes/auth";
+
+const app = new Hono();
 
 export interface Env {
     DATABASE_URL: string;
 }
 
-app.get("/", (c) => {
-    return c.text("Hello Hono!");
-});
-
-app.post("/", async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
-
-    const user = await prisma.user.create({
-        data: {
-            email: "aryan@gmail.com",
-            password: "qwerty88**",
-            role: "Admin",
-        },
-    });
-    return c.json({ user });
-});
+app.route("/auth", auth);
+app.route("");
 
 export default app;
---
+++ server/src/routes/auth.ts
@@ -0,0 +1,116 @@
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client/edge";
+import { env } from "hono/adapter";
+
+import { Hono } from "hono";
+import { sign } from "hono/jwt";
+
+const auth = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    };
+}>();
+
+auth.post("/student/signup", async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password,
+                role: "Student",
+            },
+        });
+        const token = await sign(
+            { role: user.role, email: user.email },
+            c.env.JWT_SECRET
+        );
+
+        return c.json({ msg: "success", token }, 201);
+    } catch (err: any) {
+        c.status(500);
+        return c.json({ msg: err.message });
+    }
+});
+
+auth.post("/admin/signup", async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+        const user = await prisma.user.create({
+            data: {
+                email: body.email,
+                password: body.password,
+                role: "Admin",
+            },
+        });
+        const token = await sign(
+            { role: user.role, email: user.email },
+            c.env.JWT_SECRET
+        );
+        return c.json({ msg: "success", token }, 201);
+    } catch (err: any) {
+        c.status(500);
+        return c.json({ msg: err.message });
+    }
+});
+
+auth.post("/student/login", async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email: body.email,
+                password: body.password,
+                role: "Student",
+            },
+        });
+        if (user == null) throw Error("User does not exist in DB");
+        const token = await sign(
+            { role: user.role, email: user.email },
+            c.env.JWT_SECRET
+        );
+
+        return c.json({ msg: "success", token }, 201);
+    } catch (err: any) {
+        c.status(500);
+        return c.json({ msg: err.message });
+    }
+});
+
+auth.post("/admin/login", async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+        const user = await prisma.user.findFirst({
+            where: {
+                email: body.email,
+                password: body.password,
+                role: "Admin",
+            },
+        });
+        if (user == null) throw Error("User does not exist in DB");
+        const token = await sign(
+            { role: user.role, email: user.email },
+            c.env.JWT_SECRET
+        );
+
+        return c.json({ msg: "success", token }, 201);
+    } catch (err: any) {
+        c.status(500);
+        return c.json({ msg: err.message });
+    }
+});
+
+export default auth;
