177a479ef7d5f2afe39f24a0b4bbab999bbadfc8
WiseyXD <aryan.s.nag@gmail.com>
2024-03-13 16:55:22 +0530

    build : socket server done

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -1,7 +1,13 @@
 import { Server } from "socket.io";
 
-const io = new Server(3000);
+const io = new Server(3000, {
+    // @ts-ignore
+    cors: true,
+});
 
 io.on("connection", (socket) => {
     console.log("Socket Connected", socket.id);
+    socket.on("frame", (data) => {
+        console.log(data);
+    });
 });
